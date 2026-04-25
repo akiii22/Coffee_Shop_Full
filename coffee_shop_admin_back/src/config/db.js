71ccd68b7630c97140670e0a61819d860d8ef8df
jeromee <bulosanjerome50@gmail.com>
2026-04-25 09:00:37 +0800

    Fixing the config.js

--- coffee_shop_admin_back/src/config/db.js
+++ coffee_shop_admin_back/src/config/db.js
@@ -8,7 +8,12 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+    ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+  keepAlive: true,
+
 });
 
 db.connect()
